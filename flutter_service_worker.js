bb64d82f342de5f342e78090c3246cdf324e14d6
omrankallel <omran00kallel@gmail.com>
2022-11-10 13:43:05 +0100

    'up'

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -309,9 +309,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "129d12c8bbfd1a9ef53b80fcbbdd8a7f",
-"/": "129d12c8bbfd1a9ef53b80fcbbdd8a7f",
-"main.dart.js": "d0ed7967ed0b5cd8c16f74cba7f79664",
+"index.html": "db65fdd74f3dd2d509f5fc5e58b291d5",
+"/": "db65fdd74f3dd2d509f5fc5e58b291d5",
+"main.dart.js": "664b25458fde47f06f2b15302ce78168",
 "manifest.json": "9c903e50a5c76b5f1357f9530d1147e1",
 "version.json": "e9d62b979604329eee60bce30d853e4e"
 };
